b7b31b73ec69a3779eb8bab5f459d96fd3872a89
S7ARSCR3AM <rowan.j.burgess@gmail.com>
2013-08-12 19:22:54 +1000

    Implementing ConnectionBroker as a servlet

--- Input/src/main/java/au/edu/newcastle/SENG48002013/input/ConnectionBroker.java
+++ Input/src/main/java/au/edu/newcastle/SENG48002013/input/ConnectionBroker.java
@@ -1,57 +1,34 @@
 package au.edu.newcastle.SENG48002013.input;
 
-import au.edu.newcastle.seng48002013.instructions.BaseInstruction;
-import javax.inject.Inject;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.server.ServerEndpoint;
-import org.codehaus.jackson.map.ObjectMapper;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Initial entry point for clients wanting to connect to the system. Receives a
  * registration request from the client, determines if it should be granted.
  */
-@ServerEndpoint("/entry")
-public class ConnectionBroker 
+@WebServlet("/connect")
+public class ConnectionBroker extends HttpServlet
 {
-
-    @Inject
-    SecurityManager secManager; //requires review, not working yet
-
-    @OnOpen
-    public void onOpen(Session peer) 
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException 
     {
-        System.out.println("Inside the onOpen method");
-    }
-
-    @OnMessage
-    public void onMessage(Session s, String message) 
-    {
-        System.out.println(message);
-        System.out.println("Session ID" + s.getId());
-
-        ObjectMapper mapper = new ObjectMapper();
-        BaseInstruction instruction;
-
-        try 
-        {
-            instruction = mapper.readValue(message, BaseInstruction.class);
-            System.out.println("STATS");
-            System.out.println(instruction.getId());
-            
-            if (true) //if there is room for another player
-            {
-                //have a token generated
-                //send the token back to the client 
-                s.getAsyncRemote().sendText("Reply From Connection Broker!");
-            }
-        } 
+        String mac = req.getParameter("mac");
+	String type = req.getParameter("type");
         
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
-        }
-
+        System.out.println("MAC: " + mac + " Device Type: " + type);
+        
+        // check for space in game, request token from security manager
+        resp.sendRedirect("ClientConnectRequest.jsp"); //temp
+        
     }
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+    {
+        doPost(req, resp);      
+    }
+
+   
 }
