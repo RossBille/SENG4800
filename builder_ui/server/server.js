e1317d2f6c9c4cea725c026d8eea32aaf36e39d7
jordanmkoncz <jordanmkoncz@gmail.com>
2013-08-19 23:26:47 +1000

    Changes to server

--- builder_ui/server/server.js
+++ builder_ui/server/server.js
@@ -1,5 +1,5 @@
 var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({port: 8080});
+var wss = new WebSocketServer({port: 8088});
 
 wss.on ('connection', function(socket) {
     console.log("Opened");
