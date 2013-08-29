ed0e7a59742f6976f999b71b234b0c7555b16761
b1nd <clint@celero.com.au>
2013-08-29 22:06:42 +1000

    Update action to work with predefined images

--- FrontEnd/public/js/action.js
+++ FrontEnd/public/js/action.js
@@ -1,4 +1,5 @@
 var app = app || {};
+var list = app.list || [];
 
 /* Our callback to perform action on each object */
 app.action = function(objects) {
@@ -7,18 +8,26 @@ app.action = function(objects) {
 
     while(objects.length > 0) {
         var object = objects.pop();
-        var image = new Image();
-        image.src = object.sprite;
-        image.setAtX = object.dx;
-        image.setAtY = object.dy;
-        image.onload = function () {
+        console.log(object);
+        var img = getImage(object);
+        console.log(img);
+        img.onload = function () {
             context.drawImage(this, this.setAtX, this.setAtY);
         };
     }
-};
 
-app.Image = function(id, x, y) {
-    this.id = id;
-    this.x = x;
-    this.y = y;
+    function getImage(object) {
+
+        if (list[object.id] !== undefined) {
+            var image = list[object.id];
+        }
+        else {
+            var image = new Image();
+        }
+        image.src = object.sprite;
+        image.setAtX = object.dx;
+        image.setAtY = object.dy;
+        list[object.id] = image;
+        return image;
+    }
 };
