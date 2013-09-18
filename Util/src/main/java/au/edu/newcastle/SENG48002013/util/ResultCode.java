d35a9b718486b897c85c409e0cce37959937cb63
Ross Bille <rossjbille@gmail.com>
2013-09-18 21:07:24 +1000

    [FEATURE] Case 89: Consolidated error and success codes

---
+++ Util/src/main/java/au/edu/newcastle/SENG48002013/util/ResultCode.java
@@ -0,0 +1,12 @@
+package au.edu.newcastle.SENG48002013.util;
+
+/**
+ *
+ * @author rossbille
+ */
+public class ResultCode
+{
+	public static final int SUCCESS = 1;
+	public static final int UNAUTHORISED_ACCESS = -1;
+    public static final int INSUFFICIENT_ROOM = -2;	
+}
